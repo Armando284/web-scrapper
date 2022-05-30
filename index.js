a5d7b2e8ebe6b10c2ce73469ba6d3ea49ad4215d
Armando Peña Tamayo (Ender) <ender@aleph.engineering>
2022-05-30 17:34:50 -0400

    Adding cors and body formatter

--- index.js
+++ index.js
@@ -2,24 +2,42 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 // code constants
 const PORT = 8000
 const url = 'https://www.theguardian.com/uk'
 
+// app configuration
 const app = express()
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-axios(url).then(response => {
-    const html = response.data
-    const $ = cheerio.load(html)
+const getData = async () => {
     const articles = []
-    // console.log(html);
-    $('.fc-item__title', html).each(function () {
-        const title = $(this).text()
-        const url = $(this).find('a').attr('href')
-        articles.push({ title, url })
+    await axios(url).then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        // console.log(html);
+        $('.fc-item__title', html).each(function () {
+            const title = $(this).text()
+            const url = $(this).find('a').attr('href')
+            articles.push({ title, url })
+        })
+        // console.log(articles)
+    }).catch(err => console.error(err))
+    return articles
+}
+
+app.get('/', (req, res) => {
+    getData().then(articles => {
+        res.status(200).json(articles)
+    }).catch(err => {
+        res.status(400).json({ err: err })
+        console.error(err)
     })
-    console.log(articles)
-}).catch(err => console.error(err))
+})
 
 app.listen(PORT, () => console.log(`App running on PORT ${PORT}`))
