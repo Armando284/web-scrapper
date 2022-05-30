bad9754f7417f06b65c1e8497bdb8d97479b6947
Armando Peña Tamayo (Ender) <ender@aleph.engineering>
2022-05-30 12:21:51 -0400

    First working version

--- index.js
+++ index.js
@@ -5,5 +5,21 @@ const express = require('express')
 
 // code constants
 const PORT = 8000
+const url = 'https://www.theguardian.com/uk'
 
-const app = express(PORT, () => console.log(`App running on PORT ${PORT}`))
+const app = express()
+
+axios(url).then(response => {
+    const html = response.data
+    const $ = cheerio.load(html)
+    const articles = []
+    // console.log(html);
+    $('.fc-item__title', html).each(function () {
+        const title = $(this).text()
+        const url = $(this).find('a').attr('href')
+        articles.push({ title, url })
+    })
+    console.log(articles)
+}).catch(err => console.error(err))
+
+app.listen(PORT, () => console.log(`App running on PORT ${PORT}`))
