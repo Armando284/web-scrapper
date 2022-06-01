f84b24f8e7e0838be13ad33305123f3c394b3cf2
Armando Peña Tamayo (Ender) <ender@aleph.engineering>
2022-06-01 17:48:57 -0400

    Change source from news to games
    Adding google play free games

--- index.js
+++ index.js
@@ -7,7 +7,20 @@ const bodyParser = require('body-parser')
 
 // code constants
 const PORT = 8000
-const url = 'https://www.theguardian.com/uk'
+const sources = [
+    {
+        name: 'Play Store',
+        url: 'https://play.google.com/store/games?hl=es&gl=US',
+        base: 'https://play.google.com',
+        selectors: {
+            container: '.Si6A0c',
+            title: 'span.sT93pb.DdYX5.OnEJge',
+            image: 'img.T75of.stzEZd',
+            type: 'span.sT93pb.w2kbF',
+            stars: 'span.sT93pb.CKzsaf>span.w2kbF',
+        }
+    }
+]
 
 // app configuration
 const app = express()
@@ -16,30 +29,45 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // scrapping
-const getData = async () => {
+const getData = async (source) => {
     const articles = []
-    await axios(url).then(response => {
+    await axios(source.url).then(response => {
         const html = response.data
         const $ = cheerio.load(html)
         // console.log(html);
-        $('.fc-item__title', html).each(function () {
-            const title = $(this).text()
-            const url = $(this).find('a').attr('href')
-            articles.push({ title, url })
+        $(source.selectors.container, html).each(function () {
+            const title = $(this).find(source.selectors.title).text()
+            const image = $(this).find(source.selectors.image).attr('src')
+            const type = $(this).find(source.selectors.type).text()
+            const stars = $(this).find(source.selectors.stars).text()
+            const url = $(this).attr('href')
+            articles.push({ title, image, type, stars, url: source.base + url })
         })
         // console.log(articles)
     }).catch(err => console.error(err))
-    return articles
+    return { source: source.name, games: articles }
 }
 
 // routing
+// home
 app.get('/', (req, res) => {
-    getData().then(articles => {
-        res.status(200).json(articles)
-    }).catch(err => {
-        res.status(400).json({ err: err })
-        console.error(err)
+    res.send('Welcome to my News API')
+    console.log('Welcome to my News API')
+})
+// news
+app.get('/free_games', (req, res) => {
+    sources.forEach(source => {
+        getData(source).then(articles => {
+            res.json(articles)
+        }).catch(err => {
+            res.status(400).json({ err: err })
+            console.error(err)
+        })
     })
+})
+// single source
+app.get('/news/:sourceId', async (req, res) => {
+    console.log(req.params.sourceId);
 })
 
 // server
